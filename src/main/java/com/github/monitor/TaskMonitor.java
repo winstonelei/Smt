cfe64bb001df1e88d18b44ce956eaa4e24ad83d1
winstone <wanglei0643@163.com>
2017-08-17 16:43:33 +0800

    commit for task monitor

--- src/main/java/com/github/monitor/TaskMonitor.java
+++ src/main/java/com/github/monitor/TaskMonitor.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by winstone on 2017/6/2.
@@ -46,15 +47,16 @@ public class TaskMonitor {
 
         try{
             while (true){
-                String task = this.queue.take();
-                logger.info("接受到的任务="+task);
+                String task = this.queue.poll(5L,TimeUnit.MILLISECONDS); //.take();//使用take方式会阻塞队列，所以建议使用poll，当服务停止的时候服务也会停止
+                if(null != task){
+                    logger.info("接受到的任务="+task);
+                }
             }
         }catch (Exception e){
             logger.error(e.getMessage());
         }
 
     }
-
 
     /**
      * 任务队列中添加任务
@@ -65,7 +67,7 @@ public class TaskMonitor {
         try{
             TaskMonitor sendDataMonitor = getInstance();
             logger.info("得到的任务实例"+sendDataMonitor.hashCode());
-            sendDataMonitor.queue.put(task);
+            sendDataMonitor.queue.offer(task); //.put(task);
         }catch (Exception e){
             logger.error(e.getMessage());
         }
