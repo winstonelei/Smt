5f9dbf91f54b75a1c37cd443cb6be4623a97b0e9
winstonelei <wanglei0643>
2019-05-20 11:20:49 +0800

    commit

---
+++ src/main/java/com/github/dataprocessor/DataProcessor.java
@@ -0,0 +1,28 @@
+package com.github.dataprocessor;
+
+/**
+ * 数据处理器，此项目的核心接口
+ *
+ */
+public interface DataProcessor {
+    /**
+     * 处理数据
+     *
+     * @return 任务是否全部成功
+     */
+    boolean process();
+
+    /**
+     * 处理失败的分片
+     *
+     * @return 任务是否全部成功
+     */
+    boolean processErrorSlices();
+
+    /**
+     * 恢复上次未完成的任务（断点续传）
+     *
+     * @return 任务是否全部成功
+     */
+    boolean resumeProgress();
+}
---
+++ src/main/java/com/github/dataprocessor/threadpool/NamedTaskThreadFactory.java
@@ -0,0 +1,23 @@
+package com.github.dataprocessor.threadpool;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 线程创建工厂，生产带名字的线程
+ *
+ */
+public class NamedTaskThreadFactory implements ThreadFactory {
+    private final String threadName;
+    private AtomicInteger id;
+
+    public NamedTaskThreadFactory(String threadName) {
+        this.threadName = threadName;
+        id = new AtomicInteger(0);
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        return new Thread(r, threadName + "-" + id.getAndIncrement());
+    }
+}
