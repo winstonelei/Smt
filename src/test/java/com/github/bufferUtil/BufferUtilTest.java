cf38222eab3e0f46fce15fa66907b369a0ec68db
wangl <wangl>
2020-01-02 14:55:50 +0800

    commit

--- src/test/java/com/github/bufferUtil/BufferUtilTest.java
+++ src/test/java/com/github/bufferUtil/BufferUtilTest.java
@@ -4,12 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 
 /**
  * BufferUtil单元测试
- * 
- * @author looly
  *
  */
 public class BufferUtilTest {
@@ -20,7 +17,7 @@ public class BufferUtilTest {
 		ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
 		ByteBuffer buffer2 = BufferUtil.copy(buffer, ByteBuffer.allocate(5));
-	//	Assert.assertEquals("AAABB", StrUtil.str(buffer2, Charset.defaultCharset()));
+		Assert.assertEquals("AAABB", StrUtil.utf8Str(buffer2));
 	}
 
 	@Test
@@ -29,7 +26,7 @@ public class BufferUtilTest {
 		ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
 		byte[] bs = BufferUtil.readBytes(buffer, 5);
-	//	Assert.assertEquals("AAABB", StrUtil.utf8Str(bs));
+	    Assert.assertEquals("AAABB", StrUtil.utf8Str(bs));
 	}
 
 	@Test
@@ -38,7 +35,8 @@ public class BufferUtilTest {
 		ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
 		byte[] bs = BufferUtil.readBytes(buffer, 5);
-	//	Assert.assertEquals("AAABB", StrUtil.utf8Str(bs));
+		Assert.assertEquals("AAABB", StrUtil.utf8Str(bs));
+		System.out.println(StrUtil.utf8Str(bs));
 	}
 
 	@Test
@@ -59,6 +57,69 @@ public class BufferUtilTest {
 		Assert.assertNull(line);
 
 		// 读取剩余部分
-		//Assert.assertEquals("cc", StrUtil.utf8Str(BufferUtil.readBytes(buffer)));
+		Assert.assertEquals("cc", StrUtil.utf8Str(BufferUtil.readBytes(buffer)));
 	}
+
+	@Test
+	public void readIntTest() {
+		ByteBuffer buf = ByteBuffer.allocate(1024);
+/*		buf.put((byte)123);
+		buf.put((byte)2);
+		buf.put((byte)3);
+		buf.put((byte)4);*/
+        buf.putInt(123);
+		buf.flip();
+/*		System.out.println(buf.get());
+		System.out.println(buf.get());
+		System.out.println(buf.get());
+		System.out.println(buf.get());*/
+
+		int temp = BufferUtil.parseInt(buf,4);
+		System.out.println(temp);
+
+/*		int temp2 = BufferUtil.parseInt(buf,1);
+		System.out.println(temp2);
+		int temp3 = BufferUtil.parseInt(buf,1);
+		System.out.println(temp3);
+		int temp4 = BufferUtil.parseInt(buf,1);
+		System.out.println(temp4);*/
+	}
+
+
+	@Test
+	public void readLongTest() {
+		ByteBuffer buf = ByteBuffer.allocate(1024);
+		buf.putLong(123L);
+
+		buf.flip();
+/*		System.out.println(buf.get());
+		System.out.println(buf.get());
+		System.out.println(buf.get());
+		System.out.println(buf.get());*/
+
+		Long temp = BufferUtil.parseLong(buf,8);
+		System.out.println(temp);
+
+	}
+
+	@Test
+	public void  byteTest(){
+		ByteBuffer buf = ByteBuffer.allocate(1024);
+		buf.putInt(12);
+		buf.putLong(34);
+
+		buf.flip();
+
+		byte[] dst = new byte[buf.limit()];
+		buf.get(dst);
+
+		//通过字节获取int类型数据
+		System.out.println(ByteUtil.getNumber(dst,0,4));
+
+		//通过字节获取long类型数据
+		System.out.println(ByteUtil.getNumber(dst,4,8));
+
+
+	}
+
 }
